61c796eeccf861789368aef68227ad47208a9b66
Hersheys-Sketch <harshiljoshi52773@gmail.com>
2025-09-19 22:11:43 -0400

    cream i do

---
+++ src/intro.jsx
@@ -0,0 +1,18 @@
+// easter egg ideas: toggle for medieval website, secret doggo pic, thinking monkey image (ask reza for context), i CAN make minimum wage
+//boo
+
+function Intro() {
+
+  return (
+    <>
+      {
+        <div>
+            <h1 className="name">icream</h1>
+            <img src='src\assets\icream.jpg' className='icream'></img>
+        </div>
+      }
+    </>
+  )
+}
+
+export default Intro

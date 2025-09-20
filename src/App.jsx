1c783734965b6e9cdef487bf5ed2ad8ac9a2a16f
Hersheys-Sketch <harshiljoshi52773@gmail.com>
2025-09-19 21:22:09 -0400

    icream

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// easter egg ideas: toggle for medieval website, secret doggo pic, thinking monkey image (ask reza for context), i CAN make minimum wage
+
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +30,9 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */
+        <img src='src\assets\icream.jpg' className='icream'></img>
+      }
     </>
   )
 }
